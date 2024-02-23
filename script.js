2cab69bf809d9f2ffac0858f897a5edb8d420181
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-02-23 19:27:00 +0000

    Add rainbow selection button, code not working at if else statement

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
 let gridSize = 5;
+let pretty = "shit";
+let red;
+let green;
+let blue;
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
@@ -7,44 +11,69 @@ grid.style.height = "450px";
 grid.style.width = "450px";
 grid.style.border = "solid 5px pink";
 
-const button = document.createElement('button');
-button.style.backgroundColor = "pink";
-button.style.border = "solid 3px grey";
-button.style.borderRadius = "10px";
-button.style.fontSize = "20px";
-button.style.padding = "10px 5px";
-button.style.margin = "20px";
-button.textContent = "Choose grid size";
+const gridBtn = document.createElement('button')
+gridBtn.textContent = "Choose grid size";
+const rainbowBtn = document.createElement('button');
+rainbowBtn.textContent = "Switch to rainbow";
 
-container.appendChild(button);
+container.appendChild(gridBtn);
+container.appendChild(rainbowBtn);
 container.appendChild(grid);
 
-button.addEventListener('mouseover', () => {
-    button.style.cursor = "pointer";
-});
 
+rainbowBtn.addEventListener('mouseover', () => {
+    red = Math.floor(Math.random() * 255) + 1;
+    green = Math.floor(Math.random() * 255) + 1;
+    blue = Math.floor(Math.random() * 255) + 1;
+    rainbowBtn.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+});
 
 
 function drawGrid() {
     grid.replaceChildren();
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const cellSize = 450 / gridSize;
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.style.backgroundColor = "lightgrey";
-            cell.style.height = cellSize + "px";
-            cell.style.width = cellSize + "px";
-            grid.appendChild(cell);
-            cell.addEventListener('mouseover', () => {
-                cell.style.cursor = "pointer";
-                cell.style.backgroundColor = "pink";
-            });
+    if (1 === 1) {
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                const cellSize = 450 / gridSize;
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.style.backgroundColor = "lightgrey";
+                cell.style.height = cellSize + "px";
+                cell.style.width = cellSize + "px";
+                grid.appendChild(cell);
+            };
         };
+        red = Math.floor(Math.random() * 255) + 1;
+        green = Math.floor(Math.random() * 255) + 1;
+        blue = Math.floor(Math.random() * 255) + 1;
+        cell.addEventListener('mouseover', () => {
+            cell.style.cursor = "pointer";
+            cell.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+        });
+        console.log("if statement");
     };
-};
+    else {
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                const cellSize = 450 / gridSize;
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.style.backgroundColor = "lightgrey";
+                cell.style.height = cellSize + "px";
+                cell.style.width = cellSize + "px";
+                grid.appendChild(cell);
+            };
+        };
+        cell.addEventListener('mouseover', () => {
+            cell.style.cursor = "pointer";
+            cell.style.backgroundColor = "pink";
+        console.log("else statement");
+        });
+    };
+};    
 
-button.addEventListener('click', (e) => {
+
+gridBtn.addEventListener('click', (e) => {
      gridSize = prompt("Please enter a number up to 100", "5");
     if (gridSize > 100) {
         alert("Please enter a number between 1 and 100");
@@ -53,6 +82,11 @@ button.addEventListener('click', (e) => {
         drawGrid();
     }
 
+});
+
+rainbowBtn.addEventListener('click', () => {
+    pretty = "fuck";
+    drawGrid();
 });
 
 drawGrid();
