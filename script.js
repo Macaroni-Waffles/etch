169aea4151f084ac841fcf8599aa9e83295d1a65
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-02-25 15:36:20 +0000

    Add rainbow pen functionality and add nav bar

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.container');
-let gridSize = 5;
-let pretty = "shit";
+let gridSize = 20;
 let red;
 let green;
 let blue;
 
+const nav = document.createElement('div');
 const grid = document.createElement('div');
 grid.classList.add('grid');
 grid.style.height = "450px";
@@ -14,67 +14,64 @@ grid.style.border = "solid 5px pink";
 const gridBtn = document.createElement('button')
 gridBtn.textContent = "Choose grid size";
 const rainbowBtn = document.createElement('button');
-rainbowBtn.textContent = "Switch to rainbow";
+rainbowBtn.textContent = "Rainbow fun!";
 
-container.appendChild(gridBtn);
-container.appendChild(rainbowBtn);
+container.appendChild(nav);
 container.appendChild(grid);
-
-
-rainbowBtn.addEventListener('mouseover', () => {
-    red = Math.floor(Math.random() * 255) + 1;
-    green = Math.floor(Math.random() * 255) + 1;
-    blue = Math.floor(Math.random() * 255) + 1;
-    rainbowBtn.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
-});
+nav.appendChild(gridBtn);
+nav.appendChild(rainbowBtn);
 
 
 function drawGrid() {
     grid.replaceChildren();
-    if (1 === 1) {
-        for (let i = 0; i < gridSize; i++) {
-            for (let j = 0; j < gridSize; j++) {
-                const cellSize = 450 / gridSize;
-                const cell = document.createElement('div');
-                cell.classList.add('cell');
-                cell.style.backgroundColor = "lightgrey";
-                cell.style.height = cellSize + "px";
-                cell.style.width = cellSize + "px";
-                grid.appendChild(cell);
-            };
-        };
-        red = Math.floor(Math.random() * 255) + 1;
-        green = Math.floor(Math.random() * 255) + 1;
-        blue = Math.floor(Math.random() * 255) + 1;
-        cell.addEventListener('mouseover', () => {
-            cell.style.cursor = "pointer";
-            cell.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
-        });
-        console.log("if statement");
-    };
-    else {
-        for (let i = 0; i < gridSize; i++) {
-            for (let j = 0; j < gridSize; j++) {
-                const cellSize = 450 / gridSize;
-                const cell = document.createElement('div');
-                cell.classList.add('cell');
-                cell.style.backgroundColor = "lightgrey";
-                cell.style.height = cellSize + "px";
-                cell.style.width = cellSize + "px";
-                grid.appendChild(cell);
-            };
-        };
-        cell.addEventListener('mouseover', () => {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const cellSize = 450 / gridSize;
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.backgroundColor = "lightgrey";
+            cell.style.height = cellSize + "px";
+            cell.style.width = cellSize + "px";
+            grid.appendChild(cell);
+
+            cell.addEventListener('mouseover', () => {
             cell.style.cursor = "pointer";
             cell.style.backgroundColor = "pink";
-        console.log("else statement");
-        });
+            });
+        };
     };
-};    
+};  
+
+function randomRBG() {
+    red = Math.floor(Math.random() * 255) + 1;
+    green = Math.floor(Math.random() * 255) + 1;
+    blue = Math.floor(Math.random() * 255) + 1;
+};
+
+function rainbowGrid() {
+    grid.replaceChildren();
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const cellSize = 450 / gridSize;
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.backgroundColor = "lightgrey";
+            cell.style.height = cellSize + "px";
+            cell.style.width = cellSize + "px";
+            grid.appendChild(cell);
+
+            cell.addEventListener('mouseover', () => {
+            randomRBG();
+            cell.style.cursor = "pointer";
+            cell.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+            });
+        };
+    };
+};  
 
 
 gridBtn.addEventListener('click', (e) => {
-     gridSize = prompt("Please enter a number up to 100", "5");
+     gridSize = prompt("Please enter a number up to 100", "20");
     if (gridSize > 100) {
         alert("Please enter a number between 1 and 100");
     }
@@ -85,8 +82,7 @@ gridBtn.addEventListener('click', (e) => {
 });
 
 rainbowBtn.addEventListener('click', () => {
-    pretty = "fuck";
-    drawGrid();
+    rainbowGrid();
 });
 
 drawGrid();
